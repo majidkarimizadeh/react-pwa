9d30a3d3e9782c031387c7fbeaf1eaa7139a932c
majid <mj.karimizadeh@gmail.com>
2017-09-29 19:35:04 +0330

    setting needed config, loader & plugins for webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const { resolve }       = require('path');
+const DashboardPlugin     = require('webpack-dashboard/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+
+const srcDir = resolve(__dirname, 'src')
+
+module.exports = {
+
+    entry: `${srcDir}/index.js`,
+    output: {
+        filename: 'bundle.js'
+    },
+    module:{
+        rules:[{
+            enforce: 'pre',
+            test: /\.js$/,
+            loader: 'standard-loader',
+            exclude: /node_modlues/
+        },
+        {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modlues/
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallbackLoader: 'style-loader',
+                loader: 'css-loader?modules,localIdentName=[hash:base64:6]-[name]-[local]'
+            }),
+            exclude: /node_modlues/
+        }]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:`${srcDir}/index.html`
+        }),
+        new DashboardPlugin(),
+        new ExtractTextPlugin({filename: '[name].css', allChunks: true})
+    ]
+
+}
